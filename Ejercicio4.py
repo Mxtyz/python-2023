6c7d680782d290091fe7034b1d5bd7c9ec8cb36a
Mxtyz5 <Mxtyz@users.noreply.github.com>
2023-04-11 19:37:14 -0400

    Ejercicio clase

---
+++ Ejercicio4.py
@@ -0,0 +1,19 @@
+import random
+
+# Generamos un número aleatorio entre 10 y 30 para el tamaño del arreglo
+size = random.randint(10, 30)
+
+# Creamos un arreglo aleatorio de tamaño size
+arr = [random.randint(0, 30) for i in range(size)]
+
+# Imprimimos el arreglo
+print("Arreglo generado:", arr)
+
+# Pedimos al usuario que ingrese el número a buscar
+target = int(input("Ingrese el número que desea buscar en el arreglo: "))
+
+# Buscamos el número en el arreglo
+if target in arr:
+    print(f"El número {target} se encuentra en el arreglo.")
+else:
+    print(f"El número {target} no se encuentra en el arreglo.")
