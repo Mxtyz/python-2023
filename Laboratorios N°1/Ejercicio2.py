4f701995c31e8b87faae64528c02f9fafa6fe0a6
Mxtyz5 <Mxtyz@users.noreply.github.com>
2023-04-18 10:29:15 -0400

    Nuevos archivos

--- Laboratorios N°1/Ejercicio2.py
+++ Laboratorios N°1/Ejercicio2.py
@@ -1,8 +1,8 @@
 import random
 
+#Intento de codigo provando nueva forma de hacer matrices
 filas = input("Ingrese numero de filas:")
 columnas = input("Ingrese numero de columnas:")
 
-matriz = [filas],[columnas]
-
-print(matriz)
+matriz = [filas],[columnas] 
+#No alcance a terminarlo pero tuve una nocion de como hacerlo pero ya no tenia tiempo 
