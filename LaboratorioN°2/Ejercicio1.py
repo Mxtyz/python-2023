09567a80eeab859b36738b045bfbbe4ddb3221c8
Mxtyz5 <Mxtyz@users.noreply.github.com>
2023-05-16 10:38:50 -0400

    LAbN2

--- LaboratorioN°2/Ejercicio1.py
+++ LaboratorioN°2/Ejercicio1.py
@@ -1,22 +1,24 @@
 import random
+#Se ingresan las filas y columnas de cada matriz
+filas= int(input("Ingrese la cantidad de filas: ")) 
+columnas= int(input("Ingrese la cantidad de columnas: ")) 
 
-filas = int(input("Ingrese la cantidad de filas: "))
-columnas = int(input("Ingrese la cantidad de columnas: "))
+matriz1 = [[0 for j in range(columnas)] for i in range(filas)] 
+matriz2 = [[0 for j in range(columnas)] for i in range(filas)] 
 
-matriz1 = [[0 for j in range(columnas)] for i in range(filas)]
-matriz2 = [[0 for j in range(columnas)] for i in range(filas)]
-
-for i in range(filas):
-    for j in range(columnas):
-        matriz1[i][j] = random.randint(1, 5)
-        matriz2[i][j] = random.randint(1, 5)
-
-matrizsuma = [[0 for j in range(columnas)] for i in range(filas)]
-
-for i in range(filas):
-    for j in range(columnas):
-        matrizsuma[i][j] = matriz1[i][j] + matriz2[i][j]
-
-print("Matriz de suma:")
+for i in range(filas): 
+    for j in range(columnas): 
+        matriz1[i][j] = random.randint(1, 5) 
+        matriz2[i][j] = random.randint(1, 5) 
+#se suman las dos matrices
+matrizsuma = [[0 for j in range(columnas)] for i in range(filas)] 
+  
+for i in range(filas): 
+    for j in range(columnas): 
+        matrizsuma[i][j] = matriz1[i][j] + matriz2[i][j] 
+ 
+print("La matriz de suma es:")
 for fila in matrizsuma:
     print(fila)
+#Intento sobre que la matriz se multiplique por un escalar 
+escalar = input("Ingrese un escalar entre el 1 al 10:")
--- LaboratorioN°2/Ejercicio2.dart
+++ LaboratorioN°2/Ejercicio2.dart
@@ -1,6 +1,7 @@
 import 'dart:math';
 
 void main() {
+  //Listas 1 no es aletoria, lista 2 es aleatoria
   List<int> primeralista = [3, 4, 7, 9, 8, 5, 1, 2, 5, 7];
   List<int> segundalista = [];
 
@@ -9,6 +10,7 @@ void main() {
     int numAleatorio = random.nextInt(5) + 1;
     segundalista.add(-numAleatorio);
   }
+  //Imprime las 2 listas
   print("Primera lista: $primeralista");
   print("Segunda lista: $segundalista");
 }
