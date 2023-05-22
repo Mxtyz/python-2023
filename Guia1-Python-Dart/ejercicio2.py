9fccc643acb400911aa0d8c58cb5706ab6d9e071
Mxtyz5 <Mxtyz@users.noreply.github.com>
2023-05-22 11:40:06 -0400

    Modificado

--- Guia1-Python-Dart/ejercicio2.py
+++ Guia1-Python-Dart/ejercicio2.py
@@ -1,5 +1,11 @@
 import numpy as np
-
-m = np.array([[2,3,4],[5,6,7],[8,9,1]])
+m = np.random.randint(0, 10, size=(3, 3))
+print("la matriz es:",m)
 A = np.linalg.inv(m)
 print("La matriz inversa es:",A)
+
+identidad = np.array([[1,0,0],[0,1,0],[0,0,1]])
+print(identidad)
+
+iden = m * A
+print("La Matriz identidad es:",iden)
